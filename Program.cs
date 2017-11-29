f7f98a3666eb80c2e5d529d5ea4d987a376d0193
Hagata Eduarda <hagataeduarda15@outlook.com>
2017-11-29 11:25:58 -0200

    comecando a implementar um metodo que imprime o tabuleiro

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace projetoHagata
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
         }
     }
--- tabuleiro/Cor.cs
+++ tabuleiro/Cor.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace projetoHagata.tabuleiro
+﻿namespace tabuleiro
 {
-    class Cor
+    enum Cor
     {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Laranja
     }
 }

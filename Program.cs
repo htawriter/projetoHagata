3019f7cc11309a0858cbdc04fac345f3def1ae6e
Hagata Eduarda <hagataeduarda15@outlook.com>
2017-11-29 11:43:11 -0200

    comecando a colocar pecas no tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace projetoHagata
 {
@@ -8,6 +9,10 @@ namespace projetoHagata
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new tabuleiro.Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new tabuleiro.Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new tabuleiro.Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
 
